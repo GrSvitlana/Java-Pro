cb94d96378f71e05ac457fa7a2bd797ebe40cb8a
Svitlana Graf <svetlanapss@gmail.com>
2022-10-15 00:29:36 +0200

    extends Number

--- src/main/java/hw20_calculator/Main.java
+++ src/main/java/hw20_calculator/Main.java
@@ -4,7 +4,9 @@ Develop a program that performs element-by-element summation of arrays of 10 num
 The type of numbers can be any numeric (int, double, float, ...). Implement array summation with a lambda expression.
 To add two arrays element by element is like this: {1, 2, 3} + {4, 5, 6} = {5, 7, 9}
  */
+
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,5 +33,12 @@ public class Main {
         Double[] array5 = {4.8, 5.7, 6.4, 4.6, 2.2, 3.3, 4.4};
         Double[] array6 = calculatorDouble.operation(array4, array5);
         System.out.println(Arrays.stream(array6).toList());
+
+        Integer[] a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Integer[] b = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        Integer[] result = new Integer[a.length];
+
+        IntStream.range(0, a.length).forEach(i -> result[i] = a[i] + b[i]);
+        System.out.println(Arrays.stream(result).toList());
     }
 }
