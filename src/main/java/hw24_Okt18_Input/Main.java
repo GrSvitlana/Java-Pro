c6ae88f173780bda668931e51c2e992020ca2bb7
Svitlana Graf <svetlanapss@gmail.com>
2022-10-19 12:35:55 +0200

    This method prints info about a given directory

---
+++ src/main/java/hw24_Okt18_Input/Main.java
@@ -0,0 +1,45 @@
+package hw24_Okt18_Input;
+/*
+1. Write a method fileCopy that takes two file names as input: file1 and file2. The method should make a copy of
+the contents of file1 into file2. Note: file1 necessarily exists on disk.
+
+2*. Write a method that cyclically looks through the contents of a given directory and prints information
+about all files and directories in it and its subdirectories.
+ */
+
+import java.io.*;
+
+public class Main {
+    public static final String PATH_1 = "../TelRan/Resource/file1.txt";
+    public static final String PATH_2 = "..TelRan/Resource/file2.txt";
+    public static final String PATH_3 = "../TelRan";
+
+    public static void main(String[] args) throws IOException {
+        fileCopy(PATH_1, PATH_2);
+        File catalog = new File(PATH_3);
+        contentsOfDirectory(catalog);
+    }
+
+    private static void fileCopy(String file1, String file2) throws IOException {
+        FileInputStream fileInput = new FileInputStream(file1);
+        byte[] buffer = new byte[fileInput.available()];
+        fileInput.read(buffer);
+        fileInput.close();
+
+        FileOutputStream fileOutput = new FileOutputStream(file2);
+        fileOutput.write(buffer);
+        fileOutput.flush();
+        fileOutput.close();
+    }
+
+    private static void contentsOfDirectory(File directory) {
+        if (directory.isDirectory()) {
+            for (File files : directory.listFiles()) {
+                System.out.println(files.isFile() ?
+                        "This file: " + files.getName() + " \nhas a path " + files.getAbsoluteFile() :
+                        "Open the folder " + files.getName() + "/");
+                contentsOfDirectory(files);
+            }
+        }
+    }
+}
