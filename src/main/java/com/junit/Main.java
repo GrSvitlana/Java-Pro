e6bfa246ee621cbef63fa5f403df891354c76074
Svitlana Graf <svetlanapss@gmail.com>
2022-12-06 18:43:08 +0100

    annotation

--- src/main/java/com/junit/Main.java
+++ src/main/java/com/junit/Main.java
@@ -2,6 +2,11 @@ package com.junit;
 
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql";
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
