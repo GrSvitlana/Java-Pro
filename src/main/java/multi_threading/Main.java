51179a15ec30809f936190e8df084689b6cfc53e
Svitlana Graf <svetlanapss@gmail.com>
2022-12-06 20:22:42 +0100

    threads

--- src/main/java/multi_threading/Main.java
+++ src/main/java/multi_threading/Main.java
@@ -1,15 +1,20 @@
 package multi_threading;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Write a method in which three threads write data into the file line by line (10 records each, with a period of 20 ms)
  */
 
 public class Main {
-    static private String file = "src/main/java/multi_threading/file_threads.txt";
+    static private final String file = "src/main/java/multi_threading/file_threads.txt";
 
-    public static void main(String[] args) {
-        new Threads(file, "thread_1");
-        new Threads(file, "thread_2");
-        new Threads(file, "thread_3");
+    public static void main(String[] args) throws IOException {
+            FileWriter fileWriter = new FileWriter(file, true);
+            new Threads(fileWriter, "thread_1");
+            new Threads(fileWriter, "thread_2");
+            new Threads(fileWriter, "thread_3");
+            fileWriter.flush();
     }
 }
--- src/main/java/multi_threading/Threads.java
+++ src/main/java/multi_threading/Threads.java
@@ -3,20 +3,21 @@ package multi_threading;
 import java.io.FileWriter;
 
 public class Threads extends Thread {
-    private String fileName;
+    private final FileWriter fileWriter;
 
-    public Threads(String fileName, String threadName) {
-        this.fileName = fileName;
+    public Threads(FileWriter fileWriter, String threadName) {
+        this.fileWriter = fileWriter;
         this.setName(threadName);
-        this.start();
+        this.run();
     }
 
     @Override
     public void run() {
-        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
+        try {
             for (int i = 0; i < 10; i++) {
                 fileWriter.write(this.getName() + " " + i + "\n");
                 Thread.sleep(20);
+                Thread.yield();
             }
         } catch (Exception e) {
             e.printStackTrace();
